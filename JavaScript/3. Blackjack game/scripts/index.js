05fc5cf2578f5b2ad3ddebf27ef9a6e788101fcf
T-Patricia <telekpattii99@gmail.com>
2024-10-31 19:03:11 +0100

    finished lesson
    - JavaScript fundamentals
    - declaring variables
    - creating onclick functions
    - arrays, objects, booleans, numbers, strings
    - &&, || operators
    - if else statements
    - returning value
    - randomizing numbers
    - for loop - counting

---
+++ JavaScript/3. Blackjack game/scripts/index.js
@@ -0,0 +1,82 @@
+// declaring variables
+let player = {
+    name: "T-Patricia",
+    chips: 0
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+// creating a function to randomize a card: 
+// randomize number floored between 1-13,
+// if its 1/11/12/13 returning the right value depends on the card type
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+// creating a function to start the game
+// player set to alive
+// declaring 1st and 2nd card
+// declaring cards array and a sum variable
+// indenting another function 
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+// creating a function to render the game
+// updating Cards inner text with a for loop
+// updating Sum inner text with if statement 
+// updating message element depending on player's in game performance
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+// creating a function to draw a new card
+// player needs to be alive AND does not have Blackjack
+// adding new card to sum 
+// pushing new card to array
+// indenting another function 
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+}
