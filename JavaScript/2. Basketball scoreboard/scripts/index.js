fe2bad283db0b9294056b0bf9341893e516d4d15
T-Patricia <telekpattii99@gmail.com>
2024-11-03 16:34:28 +0100

    updated script
    - added new function to highlight leading team
    - if statements
    - changing color with .js

--- JavaScript/2. Basketball scoreboard/scripts/index.js
+++ JavaScript/2. Basketball scoreboard/scripts/index.js
@@ -8,38 +8,60 @@ let resultGuest = document.getElementById("result-guest")
 function addOneHome() {
     countHome += 1
     resultHome.innerText = countHome
+    leaderHighlight()
 }
 
 function addTwoHome() {
     countHome += 2
     resultHome.innerText = countHome
+    leaderHighlight()
 }
 
 function addThreeHome() {
     countHome += 3
     resultHome.innerText = countHome
+    leaderHighlight()
 }
 
 // Guest functions
 function addOneGuest() {
     countGuest += 1
     resultGuest.innerText = countGuest
+    leaderHighlight()
 }
 
 function addTwoGuest() {
     countGuest += 2
     resultGuest.innerText = countGuest
+    leaderHighlight()
 }
 
 function addThreeGuest() {
     countGuest += 3
     resultGuest.innerText = countGuest
+    leaderHighlight()
 }
 
 // New game function
-function newgame() {
+function newGame() {
     resultGuest.innerText = 0
     resultHome.innerText = 0
     countHome = 0
     countGuest = 0
+}
+
+// Leading team highlight function
+function leaderHighlight(){
+    if (countHome > countGuest) {
+        resultHome.style.color = "green"
+        resultGuest.style.color = "#F94F6D"
+    }
+    if (countHome < countGuest) {
+        resultGuest.style.color = "green"
+        resultHome.style.color = "#F94F6D"
+    }
+    else if (countHome === countGuest) {
+        resultHome.style.color = "#9AABD8"
+        resultGuest.style.color = "#9AABD8"
+    }
 }
