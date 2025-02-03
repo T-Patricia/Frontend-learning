cec65dc15c4a353981d5d9b7090915b30682ca24
T-Patricia <telekpattii99@gmail.com>
2025-02-03 18:44:22 +0100

    updated js, css
    - fixed render()
    - only renders if there are available dogs
    - feedback if there is no available dog
    - setTimeout() to badge

--- JavaScript/21. Tindog/scripts/index.js
+++ JavaScript/21. Tindog/scripts/index.js
@@ -4,29 +4,54 @@ import dogsData from "../data/data.js"
 
 // get elements from DOM, creating variables
 const mainContainer = document.getElementById("main-container")
-const likeBtn = document.getElementById("like-btn")
-// const dislikeBtn = document.getElementById("dislike-btn")
 
 let currentDogIndex = 0
 let currentDog = new Dog(dogsData[currentDogIndex])
 
-
+// function to render the current dog
 function render() {
     mainContainer.innerHTML = currentDog.getDogHtml()
+
+    document.getElementById("like-btn").addEventListener("click", likeDog)
+    document.getElementById("dislike-btn").addEventListener("click", dislikeDog)
 }
 
+// function to get a new dow
 function getNewDog() {
-    currentDogIndex += 1
-    currentDog = new Dog(dogsData[currentDogIndex])
-    render()
+    // check first if there are more dogs available
+    if(currentDogIndex < dogsData.length - 1) {
+        currentDogIndex += 1
+        currentDog = new Dog(dogsData[currentDogIndex])
+        render()
+        // feedback if there are no dogs
+    } else {
+        mainContainer.innerHTML = `
+            <div class="feedback-container">
+                <h2 class="feedback-message">No more dogs available! 🐶</h2>
+            </div>
+        `
+    }
 }
 
-function yes() {
+// handle like action
+function likeDog() {
     currentDog.setMatchStatus(true)
-    getNewDog()
+    const likeBadge = document.getElementById("like-badge")
+    if (likeBadge) {
+        likeBadge.style.display = "block"
+        setTimeout(getNewDog, 800)
+    }
 }
 
+// handle dislike action
+function dislikeDog() {
+    currentDog.setMatchStatus(false)
+    const nopeBadge = document.getElementById("nope-badge")
+    if (nopeBadge) {
+        nopeBadge.style.display = "block"
+        setTimeout(getNewDog, 800)
+    }
+}
 
-likeBtn.addEventListener("click", yes)
-
+// initial render
 render()
